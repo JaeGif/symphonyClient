f9951dafee302fa60618ad24fdd3553f6611145b
Jae <giffordjacob0@gmail.com>
2023-03-31 08:13:23 +0800

    Front end auth routing added, non logged in routes are protected

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
 import Layout from './pages/Layout';
 import MessageLayout from './pages/MessageLayout';
 import Room from './pages/Room';
 import UserLayout from './pages/UserLayout';
 import UserProfile from './pages/UserProfile';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Error404 from './pages/Error404';
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({}); // Stores user data
+  const [isLoggedIn, setIsLoggedIn] = useState(true); // Boolean for conditionally showing login routes
+
+  const loginUser = (isGuest = false) => {};
+  const registerUser = () => {};
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='messages' element={<MessageLayout />}>
-            <Route path=':id' element={<Room />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        {isLoggedIn ? (
+          <Route path='/' element={<Layout />}>
+            <Route path='messages' element={<MessageLayout />}>
+              <Route path=':id' element={<Room />} />
+            </Route>
+            <Route path='profile' element={<UserLayout />}>
+              <Route path=':id' element={<UserProfile />} />
+            </Route>
           </Route>
-          <Route path='profile' element={<UserLayout />}>
-            <Route path=':id' element={<UserProfile />} />
-          </Route>
-        </Route>
+        ) : (
+          <Route path='*' element={<Navigate to='/login' />} />
+        )}
+        <Route path='*' element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
