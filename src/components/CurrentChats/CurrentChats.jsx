576e130605b435a1cc5bf67c45a54df0e6c8b04e
Jae <giffordjacob0@gmail.com>
2023-04-08 18:10:10 +0800

    rooms in current chat now styled, and link to the correct rooms via useQueries

--- src/components/CurrentChats/CurrentChats.jsx
+++ src/components/CurrentChats/CurrentChats.jsx
@@ -1,24 +1,58 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../../App';
-import mongoose from 'mongoose';
-import { useQuery } from '@tanstack/react-query';
-import { useNavigate } from 'react-router';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { TokenContext, UserContext } from '../../App';
+import { useQueries, useQuery } from '@tanstack/react-query';
+import { Link, useParams } from 'react-router-dom';
+import { BallTriangle } from 'react-loading-icons';
+
+const apiURL = import.meta.env.VITE_SOCKET_ADDRESS;
 
 function CurrentChats() {
   const loggedInUser = useContext(UserContext);
+  const token = useContext(TokenContext);
+  const roomParam = useParams();
+
+  const fetchRoom = async (room) => {
+    const res = await fetch(`${apiURL}/api/rooms/${room}`, {
+      mode: 'cors',
+      headers: {
+        Authorization: 'Bearer' + ' ' + token,
+      },
+    });
+    const data = await res.json();
+    return data.room;
+  };
+  const roomsQueries = useQueries({
+    queries: loggedInUser.rooms.map((room) => {
+      return {
+        queryKey: ['room', { _id: room }],
+        queryFn: () => fetchRoom(room),
+      };
+    }),
+  });
 
   return (
-    <div className='w-1/5 min-w-fit dark:bg-gray-800 shadow-sm'>
-      CurrentChats
-      {loggedInUser.rooms.map((roomId) => (
-        <div>
-          <p>Title</p>
-          <Link to={`/messages/${roomId}`}>
-            <div>_id: {roomId}</div>
+    <div className='w-1/5 min-w-fit dark:bg-gray-800 shadow-sm overflow-scroll'>
+      {roomsQueries.map((query) =>
+        query.isSuccess ? (
+          <Link to={`/messages/${query.data._id}`}>
+            <div
+              className={
+                roomParam.id == query.data._id
+                  ? `p-5 hover:bg-gray-900 cursor-pointer border-r-blue-500 border-r-4 rounded-sm`
+                  : 'p-5 hover:bg-gray-900 cursor-pointer'
+              }
+            >
+              <p className={roomParam.id == query.data._id && `text-pink-500`}>
+                {query.data.title}
+              </p>
+            </div>
           </Link>
-        </div>
-      ))}
+        ) : (
+          <div className='p-5 hover:bg-gray-900 cursor-pointer flex justify-center items-center'>
+            <BallTriangle stroke='#ec4899' className='h-6' />
+          </div>
+        )
+      )}
     </div>
   );
 }
