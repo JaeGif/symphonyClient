81dacf803e415af2558b0718424891a64d78faa3
Jae <giffordjacob0@gmail.com>
2023-04-14 20:23:36 +0800

    search for chats function enabled

--- src/components/modals/createRoom/Search.jsx
+++ src/components/modals/createRoom/Search.jsx
@@ -2,14 +2,22 @@ import React, { useContext } from 'react';
 import { TokenContext, UserContext } from '../../../App';
 const apiURL = import.meta.env.VITE_SCOKET_ADDRESS;
 
-function Search({ setQuery }) {
+function Search({ setQuery, handleSearching, setSearching }) {
   const user = useContext(UserContext);
   const token = useContext(TokenContext);
 
   return (
     <div className='min-w-[300px] max-w-[35%]'>
       <input
-        onChange={(e) => setQuery(e.target.value)}
+        onBlur={(e) => {
+          if (e.target.value === '') {
+            setTimeout(setSearching, 2000);
+          }
+        }}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          handleSearching(e);
+        }}
         className='w-full h-10 rounded-md text-lg dark:bg-gray-600 p-2 placeholder:text-gray-500'
         placeholder='Find public rooms...'
         type='text'
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -5,6 +5,7 @@ import RoomCard from '../components/details/RoomCard';
 import Search from '../components/modals/createRoom/Search';
 import { useOutlet } from 'react-router';
 import { useSearchParams } from 'react-router-dom';
+import uniqid from 'uniqid';
 
 import SearchResults from './SearchResults';
 
@@ -24,14 +25,23 @@ function Explore() {
   const token = useContext(TokenContext);
   const [searchParams, setSearchParams] = useSearchParams({});
   const [searching, setSearching] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [topicQuery, setTopicQuery] = useState('');
   const [query, setQuery] = useState('');
 
   useEffect(() => {
-    setSearchParams(`?${new URLSearchParams({ query: query })}`);
-    if (query === '') {
+    let params = {};
+    if (query) {
+      params.title = query;
+    }
+    if (topicQuery) {
+      params.topic = topicQuery;
+    }
+    setSearchParams(`?${new URLSearchParams(params)}`);
+    if (query === '' && topicQuery === '') {
       setSearchParams({});
     }
-  }, [query]);
+  }, [query, topicQuery]);
 
   const getPopular = async () => {
     const res = await fetch(`${apiURL}/api/rooms?popular=true&returnLimit=9`, {
@@ -48,7 +58,6 @@ function Explore() {
     queryFn: getPopular,
   });
   const getSearchResults = async () => {
-    console.log(searchParams);
     const res = await fetch(`${apiURL}/api/rooms?${searchParams}`, {
       mode: 'cors',
       method: 'GET',
@@ -61,6 +70,20 @@ function Explore() {
     queryKey: ['rooms', { q: query }],
     queryFn: getSearchResults,
   });
+  const handleSearching = (e) => {
+    if (e.target.value !== '') {
+      setSearching(true);
+    }
+  };
+  const handleTopicSelection = (e, i) => {
+    if (selected === i) {
+      setSelected(null);
+      setTopicQuery('');
+    } else {
+      setTopicQuery(e.target.textContent);
+      setSelected(i);
+    }
+  };
 
   return (
     <div className='p-4 flex w-full items-center flex-col overflow-scroll gap-4'>
@@ -68,13 +91,26 @@ function Explore() {
       <div className='w-full flex items-end'>
         <div>
           <p>Search:</p>
-          <Search setQuery={setQuery} />
+          <Search
+            setQuery={setQuery}
+            handleSearching={handleSearching}
+            setSearching={setSearching}
+          />
         </div>
         <div className='flex flex-shrink gap-2 w-full pl-5'>
-          {topics.map((topic) => (
+          {topics.map((topic, i) => (
             <div>
               <button
-                className={`p-2 pt-1 pb-1 bg-blue-500 rounded-md hover:bg-blue-600`}
+                key={uniqid()}
+                id={uniqid()}
+                onClick={(e) => {
+                  handleTopicSelection(e, i);
+                }}
+                className={
+                  i === selected
+                    ? `p-2 pt-1 pb-1 bg-pink-500 rounded-md hover:bg-pink-600`
+                    : `p-2 pt-1 pb-1 bg-blue-500 rounded-md hover:bg-blue-600`
+                }
               >
                 {topic}
               </button>
@@ -93,8 +129,12 @@ function Explore() {
         ) : (
           <>
             <h2>Relevant Rooms</h2>
-            {searchParams && searchRoomsQuery.data && (
+            {searchParams &&
+            searchRoomsQuery.data &&
+            searchRoomsQuery.data.length !== 0 ? (
               <SearchResults data={searchRoomsQuery.data} />
+            ) : (
+              <h1>No rooms match your search.</h1>
             )}
           </>
         )}
